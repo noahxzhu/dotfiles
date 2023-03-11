92d02abea9307d72d7321882443cde2d59e42908
Noah Zhu <virezox@gmail.com>
2023-03-11 17:04:20 +0800

    Update the logo

--- .config/nvim/lua/core/plugin_config/dap/js-ts.lua
+++ .config/nvim/lua/core/plugin_config/dap/js-ts.lua
@@ -8,14 +8,14 @@ if not dap_ok then
   return
 end
 
--- local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
-local DEBUGGER_PATH = vim.fn.expand "$HOME/GitHub/vscode-js-debug"
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+-- local DEBUGGER_PATH = vim.fn.expand "$HOME/GitHub/vscode-js-debug"
 
 dap_vscode_js.setup {
   -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
   -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-  -- debugger_path = mason_path .. "packages/js-debug-adapter",
-  debugger_path = DEBUGGER_PATH,
+  debugger_path = mason_path .. "packages/js-debug-adapter",
+  -- debugger_path = DEBUGGER_PATH,
   -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
   adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
   -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging

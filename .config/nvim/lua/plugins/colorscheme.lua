417ed976d67208c58cbaaee43fa32f988ef3cedf
Noah Zhu <noah.x.zhu@gmail.com>
2023-03-30 01:48:25 +0800

    feat(kitty): add colorscheme gruvbox-material

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,33 @@
 return {
-    -- catppuccin
-    {
-        "catppuccin/nvim",
-        lazy = false,
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme "catppuccin"
-        end
-    },
+  -- catppuccin
+  {
+    "catppuccin/nvim",
+    enabled = false,
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "catppuccin"
+    end,
+  },
+
+  -- gruvbox-material
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- For dark version.
+      vim.opt.background = "dark"
+      -- For light version.
+      -- vim.opt.background = "light"
+
+      -- Set contrast.
+      -- Available values: 'hard', 'medium'(default), 'soft'
+      vim.g.gruvbox_material_background = "hard"
+      -- For better performance
+      vim.g.gruvbox_material_better_performance = true
+      vim.cmd.colorscheme "gruvbox-material"
+    end,
+  },
 }

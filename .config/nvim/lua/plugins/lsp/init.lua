a281de001b73ff33fafe700c3985746593049dba
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-23 16:58:53 +0800

    feat(nvim): let null-ls read edition from Cargo.toml for rustfmt

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -130,7 +130,23 @@ return {
           nls.builtins.formatting.isort,
           nls.builtins.formatting.jq,
           nls.builtins.formatting.prettier,
-          nls.builtins.formatting.rustfmt,
+          nls.builtins.formatting.rustfmt.with {
+            extra_args = function(params)
+              local Path = require "plenary.path"
+              local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
+
+              if cargo_toml:exists() and cargo_toml:is_file() then
+                for _, line in ipairs(cargo_toml:readlines()) do
+                  local edition = line:match [[^edition%s*=%s*%"(%d+)%"]]
+                  if edition then
+                    return { "--edition=" .. edition }
+                  end
+                end
+              end
+              -- default edition when we don't find `Cargo.toml` or the `edition` in it.
+              return { "--edition=2021" }
+            end,
+          },
           nls.builtins.formatting.stylua,
 
           -- hover
--- .zshrc
+++ .zshrc
@@ -72,7 +72,7 @@ ZSH_THEME=""
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(git zsh-nvm)
 
 source $ZSH/oh-my-zsh.sh
 

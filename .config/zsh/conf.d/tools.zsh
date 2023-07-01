c3faf7f14088a306a41219dec24fe32c5d649a1a
Noah Zhu <noah.x.zhu@gmail.com>
2023-07-01 20:10:24 +0800

    feat(zsh): add miniconda config

--- .config/zsh/conf.d/tools.zsh
+++ .config/zsh/conf.d/tools.zsh
@@ -3,3 +3,19 @@ export PATH=$HOME/Tools/protoc-22.2/bin:$PATH
 
 # Pet 
 export PATH=$HOME/Tools/pet:$PATH
+
+# Conda
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('$HOME/Tools/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$HOME/Tools/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/Tools/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/Tools/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<

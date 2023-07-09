4cf6f0bc898db08f707a4c9c5a4f3a822c6757fb
Noah Zhu <noah.x.zhu@gmail.com>
2023-07-09 18:01:38 +0800

    feat(wezterm): update the font configuration for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,12 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font({
-	family = "Comic Code Ligatures",
+-- config.font = wezterm.font({
+-- 	family = "Comic Code Ligatures",
+-- })
+config.font = wezterm.font_with_fallback({
+	"Comic Code",
+	{ family = "Symbols Nerd Font", scale = 0.75 },
 })
 config.font_size = 14
 config.color_scheme = "Batman"

9a1c06041a1f2e037cac26dc7c6fc676e5b33910
Noah Zhu <noah.x.zhu@gmail.com>
2023-09-21 16:52:43 +0800

    feat(tmux): add the smart-tmux-session-manager

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -135,9 +135,9 @@ return {
 
           -- diagnostics
           nls.builtins.diagnostics.checkmake,
-          nls.builtins.diagnostics.codespell.with {
-            extra_args = { "--config", vim.fn.stdpath "config" .. "/.codespellrc" },
-          },
+          -- nls.builtins.diagnostics.codespell.with {
+          --   extra_args = { "--config", vim.fn.stdpath "config" .. "/.codespellrc" },
+          -- },
           nls.builtins.diagnostics.flake8,
           nls.builtins.diagnostics.golangci_lint,
           -- nls.builtins.diagnostics.markdownlint,
---
+++ .config/zsh/conf.d/tmux.zsh
@@ -0,0 +1,2 @@
+export PATH=$HOME/.tmux/plugins/t-smart-tmux-session-manager/bin:$PATH
+export T_SESSION_NAME_INCLUDE_PARENT=true

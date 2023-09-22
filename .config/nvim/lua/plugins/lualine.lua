841d17fb277fadeb0736ad74d771bc7bb97966e9
Noah Zhu <noah.x.zhu@gmail.com>
2023-09-22 11:04:07 +0800

    feat(nvim): disable the clock on lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -83,11 +83,11 @@ M = {
           { "progress", separator = " ", padding = { left = 1, right = 0 } },
           { "location", padding = { left = 0, right = 1 } },
         },
-        lualine_z = {
-          function()
-            return " " .. os.date "%R"
-          end,
-        },
+        -- lualine_z = {
+        --   function()
+        --     return " " .. os.date "%R"
+        --   end,
+        -- },
       },
     }
   end,

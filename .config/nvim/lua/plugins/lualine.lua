b4f4e3e1afcc5d94ff6284bc50e88bddc7e78115
Noah Zhu <noah.x.zhu@gmail.com>
2023-10-01 01:53:50 +0800

    feat(nvim): update lualine config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -86,8 +86,7 @@ M = {
           },
         },
         lualine_y = {
-          -- { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          -- { "location", padding = { left = 0, right = 1 } },
+          { "encoding", padding = { left = 1, right = 1 } },
         },
         lualine_z = {
           { "progress", separator = " ", padding = { left = 1, right = 0 } },

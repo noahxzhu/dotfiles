da98c5ee3d9ee124e5e54b8690a74892a9815661
Noah Zhu <noah.x.zhu@gmail.com>
2023-10-01 17:18:34 +0800

    feat(zsh): add poetry envs

--- .config/zsh/conf.d/darwin/python.zsh
+++ .config/zsh/conf.d/darwin/python.zsh
@@ -2,3 +2,6 @@
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+
+# Poetry setup
+export POETRY_VIRTUALENVS_IN_PROJECT=true

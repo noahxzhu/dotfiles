e30fd2be5d1b0070227c0d14e316e819ed629e81
Noah Zhu <noah.x.zhu@gmail.com>
2023-10-12 18:20:33 +0800

    feat(zsh): add tmux alias

--- .config/zsh/conf.d/darwin/alias.zsh
+++ .config/zsh/conf.d/darwin/alias.zsh
@@ -1,3 +1,9 @@
 # Alias
 alias lg="lazygit"
 alias gi="gitui"
+
+alias ta="tmux a"
+alias tat="tmux a -t"
+alias tk="tmux kill-server"
+alias tn="tmux new -s $(basename $PWD)"
+alias tnn="tmux new -s"

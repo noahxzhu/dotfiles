dbbd43a35d224a177861a576d25119786755fcd8
Noah Zhu <noah.x.zhu@gmail.com>
2023-10-17 14:46:22 +0800

    feat(nvim): remove the rainbow-delimiters.nvim integration for indent-blankline

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -25,14 +25,9 @@ return {
       vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
     end)
 
-    vim.g.rainbow_delimiters = { highlight = highlight }
-
     require("ibl").setup {
       indent = { highlight = highlight, char = "│", tab_char = "│" },
       exclude = { filetypes = { "help", "alpha", "dashboard", "NvimTree", "Trouble", "lazy" } },
-      scope = { highlight = highlight, exclude = { language = {} } },
     }
-
-    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
   end,
 }

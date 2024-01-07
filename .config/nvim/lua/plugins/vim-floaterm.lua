5f6d6f1943d3b0dd9a2f9b708cb16cc211922aab
Noah Zhu <noah.x.zhu@gmail.com>
2024-01-07 22:35:05 +0800

    feat(nvim): add vim-floaterm for yazi

---
+++ .config/nvim/lua/plugins/vim-floaterm.lua
@@ -0,0 +1,10 @@
+return {
+  "voldikss/vim-floaterm",
+  keys = {
+    {
+      "<leader>fy",
+      "<cmd>FloatermNew --height=2.0 --width=2.0 --wintype=float --opener=edit --title=Yazi yazi<cr>",
+      desc = "Yazi",
+    },
+  },
+}

762d4f7b28f9dab77cad673ec3b7cd857838477b
Noah Zhu <noah.x.zhu@gmail.com>
2024-01-09 22:08:54 +0800

    feat(nvim): upgrade telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.4",
+  branch = "0.1.x",
   cmd = "Telescope",
   dependencies = {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },

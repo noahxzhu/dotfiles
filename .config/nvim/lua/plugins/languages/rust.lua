9e816905094091442fbd4e8c89eabeb93867e33c
Noah Zhu <noah.x.zhu@gmail.com>
2024-01-11 01:46:58 +0800

    feat(nvim): replace rust-tools.nvim with rustaceanvim and remove rust.vim

--- .config/nvim/lua/plugins/languages/rust.lua
+++ .config/nvim/lua/plugins/languages/rust.lua
@@ -1,77 +1,49 @@
 return {
   {
-    "simrat39/rust-tools.nvim",
+    "mrcjkb/rustaceanvim",
+    version = "^3",
     ft = { "rust" },
     dependencies = {
-      "rust-lang/rust.vim",
       "mfussenegger/nvim-dap",
     },
     config = function()
-      local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
-      local codelldb_adapter = {
-        type = "server",
-        port = "${port}",
-        executable = {
-          command = mason_path .. "bin/codelldb",
-          args = { "--port", "${port}" },
-        },
-      }
-
-      local opts = {
-        tools = {
-          on_initialized = function()
-            vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
-              pattern = { "*.rs" },
-              callback = function()
-                local clients = vim.lsp.get_active_clients()
-                for _, client in pairs(clients) do
-                  if client.supports_method "textDocument/codeLens" then
-                    pcall(vim.lsp.codelens.refresh)
-                    return
+      vim.g.rustaceanvim = function()
+        return {
+          tools = {
+            on_initialized = function()
+              vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+                pattern = { "*.rs" },
+                callback = function()
+                  local clients = vim.lsp.get_active_clients()
+                  for _, client in pairs(clients) do
+                    if client.supports_method "textDocument/codeLens" then
+                      pcall(vim.lsp.codelens.refresh)
+                      return
+                    end
                   end
-                end
-              end,
-            })
-          end,
-        },
-        dap = {
-          adapter = codelldb_adapter,
-        },
-        server = {
-          on_attach = require("util").on_attach(function(client, buffer)
-            require("plugins.lsp.keymaps").on_attach(client, buffer)
-          end),
-          capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-          settings = {
-            ["rust-analyzer"] = {
-              lens = {
-                enable = true,
-              },
-              checkOnSave = {
-                enable = true,
-                command = "clippy",
+                end,
+              })
+            end,
+          },
+          server = {
+            on_attach = require("util").on_attach(function(client, buffer)
+              require("plugins.lsp.keymaps").on_attach(client, buffer)
+            end),
+            capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+            settings = {
+              ["rust-analyzer"] = {
+                lens = {
+                  enable = true,
+                },
+                checkOnSave = {
+                  enable = true,
+                  command = "clippy",
+                },
               },
             },
           },
-        },
-      }
-
-      require("rust-tools").setup(opts)
-
-      local dap = require "dap"
-      dap.adapters.codelldb = codelldb_adapter
-      dap.configurations.rust = {
-        {
-          name = "Launch file",
-          type = "codelldb",
-          request = "launch",
-          program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-          end,
-          cwd = "${workspaceFolder}",
-          stopOnEntry = false,
-        },
-      }
+        }
+      end
     end,
   },
   {

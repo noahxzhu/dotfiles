8b79a0e19c83179dee0ed118c4733091e0d7f905
Noah Zhu <noah.x.zhu@gmail.com>
2024-02-12 19:51:16 +0800

    feat(nvim): add obsidian.nvim

---
+++ .config/nvim/lua/plugins/obsidian-nvim.lua
@@ -0,0 +1,20 @@
+local workspaces = {}
+
+local vaults_json = os.getenv "OBSIDIAN_VAULTS"
+if vaults_json then
+  local vaults_table = vim.fn.json_decode(vaults_json)
+  if vaults_table then
+    for name, path in pairs(vaults_table) do
+      table.insert(workspaces, { name = name, path = path })
+    end
+  end
+end
+
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  ft = "markdown",
+  opts = {
+    workspaces = workspaces,
+  },
+}
---
+++ .config/zsh/conf.d/darwin/obsidian.zsh
@@ -0,0 +1,6 @@
+OBSIDIAN_VAULT_NOAH="$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/Noah's Vault"
+
+# Add multiple vaults if you need
+export OBSIDIAN_VAULTS="{
+  \"noah\":\"${OBSIDIAN_VAULT_NOAH}\"
+}"

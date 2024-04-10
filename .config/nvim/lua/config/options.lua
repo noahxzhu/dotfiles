c2bde4d8741a8318409bca3decc15412efbde0fb
Noah Zhu <noah.x.zhu@gmail.com>
2024-04-10 22:01:51 +0800

    feat(nvim): add custom snippets

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -42,3 +42,6 @@ opt.listchars:append { tab = "  ", trail = " " } -- table: Behaviors of the list
 -- [[ Splits ]]
 opt.splitright = true -- bool: Place new window to right of current one
 opt.splitbelow = true -- bool: Place new window below the current one
+
+-- [[ Snippets ]]
+opt.runtimepath:append "~/.config/nvim/snippets"
